131700aea20b8787876629bb5fcf70851db689d8
mdnuruzzamannirob <dev.mdnuruzzaman@gmail.com>
2026-07-22 13:32:35 +0600

    feat: add ESLint configuration and update package.json scripts
    - Added ESLint configuration file to enforce coding standards.
    - Updated package.json to include linting scripts for code quality checks.
    - Modified server.js to improve console log formatting.

--- app.test.js
+++ app.test.js
@@ -3,9 +3,9 @@ const { describe, it, expect } = require('@jest/globals');
 const app = require('./app');
 
 describe('GET /', () => {
-    it(`should return 'Works on my machine.'`, async () => {
-        const res = await request(app).get('/').expect(200);
+  it(`should return 'Works on my machine.'`, async () => {
+    const res = await request(app).get('/').expect(200);
 
-        expect(res.text).toBe('Works on my machine.');
-    });
+    expect(res.text).toBe('Works on my machine.');
+  });
 });
